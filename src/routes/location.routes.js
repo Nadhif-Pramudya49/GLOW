1b809770dbc01b0b09ac1649a9cb71c9feb63501
Nadhif Pramudya <24523179@students.uii.ac.id>
2026-06-17 22:20:05 +0700

    Cleanup legacy structure dan finalisasi arsitektur client-server(Migrasi dan atur ulang struktur folder secara menyeluruh)

--- src/routes/location.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationController = require('../controllers/location.controller');
-
-router.get('/', locationController.getAllLocations);
-router.get('/:id', locationController.getLocationById);
-
-module.exports = router;
